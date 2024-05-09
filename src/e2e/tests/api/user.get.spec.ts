065d6fb271cbee7d757fb1cfb75022e554a9502d
Hung Duong <huduong@launchcg.com>
2024-05-09 11:28:02 -0700

    HungDuong: Update api

--- src/e2e/tests/api/user.get.spec.ts
+++ src/e2e/tests/api/user.get.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Users GET", () => {
   test("should get user details", async ({ request }) => {
-    const response = await request.get("https://reqres.in/api/users/2");
+    const response = await request.get("https://flaskwebapptest2024.azurewebsites.net/tests");
     expect(response.status()).toBe(200);
   });
 });
