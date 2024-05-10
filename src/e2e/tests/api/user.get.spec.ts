aac468e50354cdc1c276dd41aec0f95b348ac0c1
Hung Duong <huduong@launchcg.com>
2024-05-10 14:53:54 -0700

    HungDuong: change verify point

--- src/e2e/tests/api/user.get.spec.ts
+++ src/e2e/tests/api/user.get.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from "@playwright/test";
 test.describe("Users GET", () => {
   test("should get user details", async ({ request }) => {
     const response = await request.get("https://flaskwebapptest2024.azurewebsites.net/tests");
-    expect(response.status()).toBe(200);
+    expect(response.status()).toBe(400);
   });
 });
